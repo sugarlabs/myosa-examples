2abb24fafeba748281d5dc16732cda3102dc8418
James Simmons <jim@simmons.olpc>
2010-01-10 18:04:43 -0600

    new file:   ReadEtextsTTS.py

---
+++ ReadEtextsTTS.py
@@ -0,0 +1,347 @@
+#! /usr/bin/env python
+import sys
+import os
+import zipfile
+import pygtk
+import gtk
+import getopt
+import pango
+import gobject
+import time
+import threading
+
+speech_supported = True
+
+try:
+    import gst
+    gst.element_factory_make('espeak')
+    from speech_gst import *
+except Exception, e:
+    speech_supported = False
+
+try:
+    import speechd
+except:
+    speech_supported = False
+
+voice = None
+pitch = 0
+rate = 0
+
+highlight_cb = None
+reset_cb = None
+
+page=0
+PAGE_SIZE = 45
+done = True
+
+
+class ReadEtextsActivity():
+    def __init__(self):
+        "The entry point to the Activity"
+        gtk.gdk.threads_init()
+        self.paused = False
+
+def _message_cb(bus, message, pipe):
+    if message.type in (gst.MESSAGE_EOS, gst.MESSAGE_ERROR):
+        pipe.set_state(gst.STATE_NULL)
+        if pipe is play_speaker[1]:
+            speech.reset_cb()
+    elif message.type == gst.MESSAGE_ELEMENT and \
+            message.structure.get_name() == 'espeak-mark':
+        mark = message.structure['mark']
+        speech.highlight_cb(int(mark))
+
+def _create_pipe():
+    pipe = gst.Pipeline('pipeline')
+
+    source = gst.element_factory_make('espeak', 'source')
+    pipe.add(source)
+
+    sink = gst.element_factory_make('autoaudiosink', 'sink')
+    pipe.add(sink)
+    source.link(sink)
+
+    bus = pipe.get_bus()
+    bus.add_signal_watch()
+    bus.connect('message', _message_cb, pipe)	
+
+    return (source, pipe)
+
+def _speech(speaker, words):
+    speaker[0].props.pitch = speech.pitch
+    speaker[0].props.rate = speech.rate
+    speaker[0].props.voice = speech.voice[1]
+    speaker[0].props.text = words;
+    speaker[1].set_state(gst.STATE_NULL)
+    speaker[1].set_state(gst.STATE_PLAYING)
+
+info_speaker = _create_pipe()
+play_speaker = _create_pipe()
+play_speaker[0].props.track = 2
+
+def voices():
+    return info_speaker[0].props.voices
+
+def say(words):
+    _speech(info_speaker, words)
+
+def play(words):
+    _speech(play_speaker, words)
+
+def is_stopped():
+    for i in play_speaker[1].get_state():
+        if isinstance(i, gst.State) and i == gst.STATE_NULL:
+            return True
+    return False
+
+def stop():
+    play_speaker[1].set_state(gst.STATE_NULL)
+    def highlight_next_word(self, word_count):
+        if word_count < len(self.word_tuples):
+            word_tuple = self.word_tuples[word_count]
+            textbuffer = self.textview.get_buffer()
+            tag = textbuffer.create_tag()
+            tag.set_property('weight', pango.WEIGHT_BOLD)
+            tag.set_property( 'foreground', "white")
+            tag.set_property( 'background', "black")
+            iterStart = textbuffer.get_iter_at_offset(word_tuple[0])
+            iterEnd = textbuffer.get_iter_at_offset(word_tuple[1])
+            bounds = textbuffer.get_bounds()
+            textbuffer.remove_all_tags(bounds[0], bounds[1])
+            textbuffer.apply_tag(tag, iterStart, iterEnd)
+            v_adjustment = self.scrolled_window.get_vadjustment()
+            max = v_adjustment.upper - v_adjustment.page_size
+            max = max * word_count
+            max = max / len(self.word_tuples)
+            v_adjustment.value = max
+        return True
+    
+    def keypress_cb(self, widget, event):
+        "Respond when the user presses one of the arrow keys"
+        global done
+        global speech_supported
+        keyname = gtk.gdk.keyval_name(event.keyval)
+        if keyname == 'KP_End' and speech_supported:
+            if (done):
+                self.et = EspeakThread()
+                self.et.set_words_on_page(self.words_on_page)
+                self.et.set_activity(self)
+                self.et.start()
+            else:
+                if self.paused == True:
+                    self.paused = False
+                    self.et.resume()
+                else:
+                    self.paused = True
+                    self.et.pause()
+            return True
+        if keyname == 'plus':
+            self.font_increase()
+            return True
+        if keyname == 'minus':
+            self.font_decrease()
+            return True
+        if done == False:
+            return True
+        if keyname == 'KP_Right':
+            self.page_next()
+            return True
+        if keyname == 'Page_Up' or keyname == 'KP_Up':
+            self.page_previous()
+            return True
+        if keyname == 'KP_Left':
+            self.page_previous()
+            return True
+        if keyname == 'Page_Down' or keyname == 'KP_Down':
+            self.page_next()
+            return True
+        if keyname == 'Up':
+            self.scroll_up()
+            return True
+        if keyname == 'Down':
+            self.scroll_down()
+            return True
+        return False
+
+    def page_previous(self):
+        global page
+        page=page-1
+        if page < 0: page=0
+        self.show_page(page)
+        v_adjustment = self.scrolled_window.get_vadjustment()
+        v_adjustment.value = v_adjustment.upper - v_adjustment.page_size
+
+    def page_next(self):
+        global page
+        page=page+1
+        if page >= len(self.page_index): page=0
+        self.show_page(page)
+        v_adjustment = self.scrolled_window.get_vadjustment()
+        v_adjustment.value = v_adjustment.lower
+
+    def font_decrease(self):
+        font_size = self.font_desc.get_size() / 1024
+        font_size = font_size - 1
+        if font_size < 1:
+            font_size = 1
+        self.font_desc.set_size(font_size * 1024)
+        self.textview.modify_font(self.font_desc)
+
+    def font_increase(self):
+        font_size = self.font_desc.get_size() / 1024
+        font_size = font_size + 1
+        self.font_desc.set_size(font_size * 1024)
+        self.textview.modify_font(self.font_desc)
+
+    def scroll_down(self):
+        v_adjustment = self.scrolled_window.get_vadjustment()
+        if v_adjustment.value == v_adjustment.upper - v_adjustment.page_size:
+            self.page_next()
+            return
+        if v_adjustment.value < v_adjustment.upper - v_adjustment.page_size:
+            new_value = v_adjustment.value + v_adjustment.step_increment
+            if new_value > v_adjustment.upper - v_adjustment.page_size:
+                new_value = v_adjustment.upper - v_adjustment.page_size
+            v_adjustment.value = new_value
+
+    def scroll_up(self):
+        v_adjustment = self.scrolled_window.get_vadjustment()
+        if v_adjustment.value == v_adjustment.lower:
+            self.page_previous()
+            return
+        if v_adjustment.value > v_adjustment.lower:
+            new_value = v_adjustment.value - v_adjustment.step_increment
+            if new_value < v_adjustment.lower:
+                new_value = v_adjustment.lower
+            v_adjustment.value = new_value
+
+    def show_page(self, page_number):
+        global PAGE_SIZE, current_word
+        position = self.page_index[page_number]
+        self.etext_file.seek(position)
+        linecount = 0
+        label_text = ''
+        textbuffer = self.textview.get_buffer()
+        while linecount < PAGE_SIZE:
+            line = self.etext_file.readline()
+            label_text = label_text + unicode(line, 'iso-8859-1')
+            linecount = linecount + 1
+        textbuffer.set_text(label_text)
+        self.textview.set_buffer(textbuffer)
+        self.prepare_highlighting(label_text)
+
+    def prepare_highlighting(self, label_text):
+        i = 0
+        j = 0
+        word_begin = 0
+        word_end = 0
+        current_word = 0
+        self.word_tuples = []
+        omitted = [' ', '_', u'\r', '\n', '.', ',', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '~', '<', '>', ':', ';', '+', '=', '{', '}', '[', ']', '/', '?', '"' ]
+        omitted_chars = set(omitted)
+        while i < len(label_text):
+            if label_text[i] not in omitted_chars:
+                word_begin = i
+                j = i
+                while  j < len(label_text) and label_text[j] not in omitted_chars:
+                    j = j + 1
+                    word_end = j
+                    i = j
+                word_tuple = (word_begin, word_end, label_text[word_begin: word_end].strip())
+                if word_tuple[2] != u'\r':
+                    self.word_tuples.append(word_tuple)
+            i = i + 1
+        self.words_on_page = self.add_word_marks()
+        print self.words_on_page
+
+    def add_word_marks(self):
+        "Adds a mark between each word of text."
+        i = 0
+        marked_up_text  = '<speak> '
+        while i < len(self.word_tuples):
+            word_tuple = self.word_tuples[i]
+            marked_up_text = marked_up_text + '<mark name="' + str(i) + '"/>' + word_tuple[2]
+            i = i + 1
+        return marked_up_text + '</speak>'
+
+    def save_extracted_file(self, zipfile, filename):
+        "Extract the file to a temp directory for viewing"
+        filebytes = zipfile.read(filename)
+        f = open("/tmp/" + filename, 'w')
+        try:
+            f.write(filebytes)
+        finally:
+            f.close
+
+    def read_file(self, filename):
+        "Read the Etext file"
+        global PAGE_SIZE
+        
+        if zipfile.is_zipfile(filename):
+            self.zf = zipfile.ZipFile(filename, 'r')
+            self.book_files = self.zf.namelist()
+            self.save_extracted_file(self.zf, self.book_files[0])
+            currentFileName = "/tmp/" + self.book_files[0]
+        else:
+            currentFileName = filename
+            
+        self.etext_file = open(currentFileName,"r")
+        self.page_index = [ 0 ]
+        linecount = 0
+        while self.etext_file:
+            line = self.etext_file.readline()
+            if not line:
+                break
+            linecount = linecount + 1
+            if linecount >= PAGE_SIZE:
+                position = self.etext_file.tell()
+                self.page_index.append(position)
+                linecount = 0
+        if filename.endswith(".zip"):
+            os.remove(currentFileName)
+
+    def write_file(self, file_path):
+        "Save meta data for the file."
+    
+    def delete_cb(self, widget, event, data=None):
+        return False
+    
+    def destroy_cb(self, widget, data=None):
+        gtk.main_quit()
+
+    def main(self, file_path):
+        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        self.window.connect("delete_event", self.delete_cb)
+        self.window.connect("destroy", self.destroy_cb)
+        self.window.set_title("Read Etexts Activity")
+        self.window.set_size_request(800, 600)
+        self.window.set_border_width(0)
+        self.read_file(file_path)
+        self.scrolled_window = gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
+        self.textview = gtk.TextView()
+        self.textview.set_editable(False)
+        self.textview.set_left_margin(50)
+        self.textview.set_cursor_visible(False)
+        self.textview.connect("key_press_event", self.keypress_cb)
+        buffer = self.textview.get_buffer()
+        self.font_desc = pango.FontDescription("sans 12")
+        font_size = self.font_desc.get_size()
+        self.textview.modify_font(self.font_desc)
+        self.show_page(0)
+        self.scrolled_window.add(self.textview)
+        self.window.add(self.scrolled_window)
+        self.textview.show()
+        self.scrolled_window.show()
+        v_adjustment = self.scrolled_window.get_vadjustment()
+        self.window.show()
+        gtk.main()
+      
+if __name__ == "__main__":
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "")
+        ReadEtextsActivity().main(args[0])
+    except getopt.error, msg:
+        print msg
+        print "This program has no options"
+        sys.exit(2)
