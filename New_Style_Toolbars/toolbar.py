022a85b2951ac46f0b4540af672f54cb0787f982
James Simmons <jim@olpc2.simmons>
2010-03-05 12:03:25 -0600

    Reorganize project so that each Activity version has its own directory.

---
+++ New_Style_Toolbars/toolbar.py
@@ -0,0 +1,138 @@
+# toolbar.,py  The toolbars used by ReadEtextsActivity.
+#
+# Copyright (C) 2010, James Simmons.
+# Adapted from code Copyright (C) Red Hat Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from gettext import gettext as _
+import re
+
+import pango
+import gobject
+import gtk
+
+from sugar.graphics.toolbutton import ToolButton
+
+class ReadToolbar(gtk.Toolbar):
+    __gtype_name__ = 'ReadToolbar'
+
+    def __init__(self):
+        gtk.Toolbar.__init__(self)
+
+        self.back = ToolButton('go-previous')
+        self.back.set_tooltip(_('Back'))
+        self.back.props.sensitive = False
+        self.insert(self.back, -1)
+        self.back.show()
+
+        self.forward = ToolButton('go-next')
+        self.forward.set_tooltip(_('Forward'))
+        self.forward.props.sensitive = False
+        self.insert(self.forward, -1)
+        self.forward.show()
+
+        num_page_item = gtk.ToolItem()
+
+        self.num_page_entry = gtk.Entry()
+        self.num_page_entry.set_text('0')
+        self.num_page_entry.set_alignment(1)
+        self.num_page_entry.connect('insert-text',
+                                     self.num_page_entry_insert_text_cb)
+
+        self.num_page_entry.set_width_chars(4)
+
+        num_page_item.add(self.num_page_entry)
+        self.num_page_entry.show()
+
+        self.insert(num_page_item, -1)
+        num_page_item.show()
+
+        total_page_item = gtk.ToolItem()
+
+        self.total_page_label = gtk.Label()
+
+        label_attributes = pango.AttrList()
+        label_attributes.insert(pango.AttrSize(14000, 0, -1))
+        label_attributes.insert(pango.AttrForeground(65535, 65535, 65535, 0, -1))
+        self.total_page_label.set_attributes(label_attributes)
+
+        self.total_page_label.set_text(' / 0')
+        total_page_item.add(self.total_page_label)
+        self.total_page_label.show()
+
+        self.insert(total_page_item, -1)
+        total_page_item.show()
+
+    def num_page_entry_insert_text_cb(self, entry, text, length, position):
+        if not re.match('[0-9]', text):
+            entry.emit_stop_by_name('insert-text')
+            return True
+        return False
+
+    def update_nav_buttons(self):
+        current_page = self.current_page
+        self.back.props.sensitive = current_page > 0
+        self.forward.props.sensitive = \
+            current_page < self.total_pages - 1
+        
+        self.num_page_entry.props.text = str(current_page + 1)
+        self.total_page_label.props.label = \
+            ' / ' + str(self.total_pages)
+
+    def set_total_pages(self, pages):
+        self.total_pages = pages
+        
+    def set_current_page(self, page):
+        self.current_page = page
+        self.update_nav_buttons()
+
+class ViewToolbar(gtk.Toolbar):
+    __gtype_name__ = 'ViewToolbar'
+
+    __gsignals__ = {
+        'needs-update-size': (gobject.SIGNAL_RUN_FIRST,
+                              gobject.TYPE_NONE,
+                              ([])),
+        'go-fullscreen': (gobject.SIGNAL_RUN_FIRST,
+                          gobject.TYPE_NONE,
+                          ([]))
+    }
+
+    def __init__(self):
+        gtk.Toolbar.__init__(self)
+        self.zoom_out = ToolButton('zoom-out')
+        self.zoom_out.set_tooltip(_('Zoom out'))
+        self.insert(self.zoom_out, -1)
+        self.zoom_out.show()
+
+        self.zoom_in = ToolButton('zoom-in')
+        self.zoom_in.set_tooltip(_('Zoom in'))
+        self.insert(self.zoom_in, -1)
+        self.zoom_in.show()
+
+        spacer = gtk.SeparatorToolItem()
+        spacer.props.draw = False
+        self.insert(spacer, -1)
+        spacer.show()
+
+        self.fullscreen = ToolButton('view-fullscreen')
+        self.fullscreen.set_tooltip(_('Fullscreen'))
+        self.fullscreen.connect('clicked', self.fullscreen_cb)
+        self.insert(self.fullscreen, -1)
+        self.fullscreen.show()
+
+    def fullscreen_cb(self, button):
+        self.emit('go-fullscreen')
